a121a33f83ac5c7609492a1772802773b43b9c27
Hitendra Paranjpe <hitendraparanjpe@Hitendras-MacBook-Air.local>
2026-04-13 22:07:12 +0530

    fix firebase db export

--- habit-tracker/src/firebase.js
+++ habit-tracker/src/firebase.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDTY-SbffD9RammESLKZIglC_RT_ivKDcM",
-  authDomain: "habit-tracker-47b9f.firebaseapp.com",
-  projectId: "habit-tracker-47b9f",
-  storageBucket: "habit-tracker-47b9f.firebasestorage.app",
-  messagingSenderId: "1070401085093",
-  appId: "1:1070401085093:web:69fcfc723dceb233d6f74c"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// ✅ THIS IS IMPORTANT
-export const db = getFirestore(app);
+// ✅ MUST EXPORT LIKE THIS
+const db = getFirestore(app);
+
+export { db };
