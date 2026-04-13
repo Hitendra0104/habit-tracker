44791e71b2a1ed3e5393d9bb26d851bddee0fe36
Hitendra Paranjpe <hitendraparanjpe@Hitendras-MacBook-Air.local>
2026-04-13 22:32:29 +0530

    fix firebase syntax error

--- habit-tracker/src/firebase.js
+++ habit-tracker/src/firebase.js
@@ -1,24 +1,17 @@
 import { initializeApp } from "firebase/app";
-<<<<<<< HEAD
 import { getFirestore } from "firebase/firestore";
-=======
-import { getFirestore } from "firebase/firestore"; // ✅ ADD THIS
->>>>>>> dev
 
 const firebaseConfig = {
   apiKey: "AIzaSyDTY-SbffD9RammESLKZIglC_RT_ivKDcM",
   authDomain: "habit-tracker-47b9f.firebaseapp.com",
   projectId: "habit-tracker-47b9f",
+  storageBucket: "habit-tracker-47b9f.firebasestorage.app",
+  messagingSenderId: "1070401085093",
+  appId: "1:1070401085093:web:69fcfc723dceb233d6f74c"
 };
 
 const app = initializeApp(firebaseConfig);
 
-<<<<<<< HEAD
-// ✅ MUST EXPORT LIKE THIS
 const db = getFirestore(app);
 
-=======
-// ✅ ADD THESE 2 LINES
-const db = getFirestore(app);
->>>>>>> dev
 export { db };
